60031a524200541a10772e43ef52cba1c501bc6a
Reaking1 <reabetsweramoswane@gmail.com>
2024-07-20 21:27:29 -0200

    addtion

---
+++ my-gaming-team-website/src/Footer/Footersection.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer: React.FC = () => {
+  return (
+    <div className="section">
+     <h1>This just a plain section fro niow</h1>
+    </div>
+  
+  );
+};
+
+export default Footer;
--- my-gaming-team-website/src/home section/home.tsx
+++ my-gaming-team-website/src/home section/home.tsx
@@ -3,8 +3,10 @@ import './Home.css';
 import MK1 from '../assets/Mortal kombat 1.jpg'
 import FC24 from '../assets/fc-24-gameplay-demo.jpg'
 import Apex from '../assets/1150018.jpg'
+import Cod from '../assets/¿Qué computadora elegir para jugar Call of Duty_ Warzone sin romper el bolsillo_.jpg'
 import gsap from "gsap";
 import { Link } from "react-router-dom";
+import Footer from "../Footer/Footersection";
 
 const Home: React.FC = () => {
 
@@ -30,41 +32,29 @@ const scrollingRef = useRef<HTMLDivElement>(null)
                 <img src={MK1} alt="mortal" />
                 <img src={FC24} alt="fc24 hal" />
                 <img src={Apex} alt="apex" />
+                <img src={Cod} alt="cod" />
                 <img src={MK1} alt="mortal" />
                 <img src={FC24} alt="fc24 hal" />
                 <img src={Apex} alt="apex" />
-               
+                <img src={Cod} alt="cod" /> 
             </div>
          </div>
-            <main>
-                <h1>Welcome to OutKast Gaming Team</h1>
-             <div className="cards-section">
-                <div className="card">
-                    <img src="" alt="d" />
-                    <h2>About Team</h2>
-                    <p>Learn more about our team and its memebers.</p>
-                    <Link to="/about" className="card-link">Read More</Link>
-                </div>
-                <div className="card">
-                    <img src="" alt="d" />
-                    <h2>Teams Info</h2>
-                    <p>Get to know the players and their roles.</p>
-                    <Link to="/teams" className="card-link">Read More</Link>
-                </div>
-                <div className="card">
-                    <img src="" alt="d" />
-                    <h2>Upcoming Sesssions</h2>
-                    <p>Stay updated with our upcoming gaming sessions.</p>
-                    <Link to="/upcomingsessions" className="card-link">Read More</Link>
-                </div>
-                <div className="card">
-                    <img src="" alt="d" />
-                    <h2>Contact</h2>
-                    <p>Contact us for any queries or information.</p>
-                    <Link to="/contact" className="card-link">Read More</Link>
-                </div>
-             </div>
-            </main>
+         <main>
+        <section className="intro-section">
+          <h1>Welcome to OutKast Gaming Team</h1>
+          <p>
+          We are a team of passionate gamers dedicated to competing at the highest level in
+            Apex Legends, FC24, Mortal Kombat 1, and Call of Duty. Our team is comprised of skilled players who
+            strive to push the boundaries of what's possible in gaming. Join us as we take on
+            new challenges, participate in tournaments, and grow together as a community.
+          </p>
+          <Link to="/about" className="read-more-link">Read More</Link>
+        </section>
+      </main>
+
+      <div className="footer">
+         <Footer />
+      </div>
         </div>
     );
 }
