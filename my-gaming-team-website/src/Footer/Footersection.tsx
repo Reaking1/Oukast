953e031b8e3a0fc045af6291b76ac2548c3680f1
Reaking1 <reabetsweramoswane@gmail.com>
2024-07-26 13:20:33 -0200

    updated

--- my-gaming-team-website/src/Footer/Footersection.tsx
+++ my-gaming-team-website/src/Footer/Footersection.tsx
@@ -3,6 +3,7 @@ import './Footer.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faInstagram, faTwitch, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import MapComponent from '../Map/MapComponent';
+import { Link } from 'react-router-dom';
 
 
 const Footer: React.FC = () => {
@@ -15,19 +16,35 @@ const Footer: React.FC = () => {
     <div className="footer-teams">
       <h2>Teams</h2>
       <ul>
+        <Link to="/teamsinfo">
         <li>Apex Legends</li>
+        </Link>
+        <Link to="/teamsinfo">
         <li>FC24</li>
+        </Link>
+        <Link  to="/teamsinfo">
         <li>Mortal Kombat 1</li>
+        </Link>
+        <Link  to="/teamsinfo">
         <li>Call of Duty</li>
+        </Link>
       </ul>
     </div>
-    <div className="footer-socails">
+    <div className="footer-socials">
       <h2>Follow US</h2>
       <div className="social-icons">
+        <a href='https://x.com/ghostkingmd?t=cQpWnr0d1IqL22PIscBTVA&s=09'>
         <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a  href='https://www.instagram.com/theghostking2003?igsh=MWx6eGR3d3o0NGl0cA=='>
         <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a  href='https://youtube.com/@terrornoisedivision3077?si=NMFjTcebqIEqMCEh'>
         <FontAwesomeIcon icon={faYoutube} />
+        </a>
+        <a  href='https://x.com/ghostkingmd?t=cQpWnr0d1IqL22PIscBTVA&s=09'>
         <FontAwesomeIcon icon={faTwitch} />
+        </a>
       </div>
     </div>
    </footer>
--- my-gaming-team-website/src/home section/home.tsx
+++ my-gaming-team-website/src/home section/home.tsx
@@ -36,12 +36,12 @@ const scrollingRef = useRef<HTMLDivElement>(null)
                 <img src={MK1} alt="mortal" />
                 <img src={FC24} alt="fc24 hal" />
                 <img src={Apex} alt="apex" />
-                <img src={Cod} alt="cod" /> 
+                <img src={Cod} alt="codh" /> 
             </div>
          </div>
          <main>
         <section className="intro-section">
-          <h1>Welcome to OutKast Gaming Team</h1>
+          <h2>Welcome to OutKast Gaming Team</h2>
           <p>
           We are a team of passionate gamers dedicated to competing at the highest level in
             Apex Legends, FC24, Mortal Kombat 1, and Call of Duty. Our team is comprised of skilled players who
@@ -49,6 +49,14 @@ const scrollingRef = useRef<HTMLDivElement>(null)
             new challenges, participate in tournaments, and grow together as a community.
           </p>
           <Link to="/about" className="read-more-link">Read More</Link>
+        </section>
+
+        <section className="events-section">
+          <h2>Upcoming Events</h2>
+          <p>
+          Stay tuned for our upcoming streams and local tournaments! Catch us live on Twitch and YouTube as we compete in exciting matches and events.
+          </p>
+          <Link to="/events" className="read-more-link">Learn More</Link>
         </section>
       </main>
 
