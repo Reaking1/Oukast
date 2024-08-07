1bfa75b125c607dc6dfd19a4003716291b588180
Reaking1 <reabetsweramoswane@gmail.com>
2024-08-07 13:47:02 -0200

    1 error to 1 error

---
+++ my-gaming-team-website/Backend/server.cjs
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
